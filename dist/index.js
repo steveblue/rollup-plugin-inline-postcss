1fe0344d93629982d20adbf4d0294d66bc8c7476
Steve Belovarich <steveblue@gmail.com>
2019-01-01 13:14:21 -0800

    fix issues with build

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // rollup-plugin-inline-postcss.js
-const path_1 = require("path");
-const postcss_1 = require("postcss");
-const findup_1 = require("findup");
+const path = require("path");
+const postcss = require("postcss");
+const findup = require("findup");
 const rollup_pluginutils_1 = require("rollup-pluginutils");
 function inlinePostCSS(options = {}) {
     const filter = rollup_pluginutils_1.createFilter(options.include, options.exclude);
@@ -16,20 +16,20 @@ function inlinePostCSS(options = {}) {
             if (!code.match(styleRegex))
                 return;
             try {
-                const configFile = findup_1.default.sync(__dirname, 'postcss.config.js');
-                const config = require(configFile)({
+                const configFolder = findup.sync(__dirname, 'postcss.config.js');
+                const config = require(path.join(configFolder, 'postcss.config.js'))({
                     env: process.env.NODE_ENV
                 });
                 const css = code.match(styleRegex)[0].split('`')[1];
-                const opts = { from: options.from ? path_1.default.join(process.cwd(), options.from) : id,
-                    to: options.to ? path_1.default.join(process.cwd(), options.to) : id,
+                const opts = { from: options.from ? path.join(process.cwd(), options.from) : id,
+                    to: options.to ? path.join(process.cwd(), options.to) : id,
                     map: {
                         inline: false,
                         annotation: false
                     }
                 };
                 const outputConfig = Object.keys(config.plugins).filter(key => config.plugins[key]).map(key => require(key));
-                return postcss_1.default(outputConfig)
+                return postcss(outputConfig)
                     .process(css, opts)
                     .then(result => {
                     code = code.replace(styleRegex, `css\`${result.css}\``);
