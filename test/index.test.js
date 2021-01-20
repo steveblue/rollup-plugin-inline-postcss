a28967e1348ea035423f2e05a2ab5e7d48b84434
Steve Belovarich <steveblue@gmail.com>
2021-01-19 23:30:24 -0800

    feat: version bump all dependencies

--- test/index.test.js
+++ test/index.test.js
@@ -5,15 +5,10 @@ const rollup = require('./../node_modules/rollup/dist/rollup').rollup;
 const inlinePostCSS = require('./../dist/index').default;
 
 function fixture(...args) {
-  return path.join(__dirname, 'fixtures', ...args)
+  return path.join(__dirname, 'fixtures', ...args);
 }
 
-async function write({
-  input,
-  outDir,
-  options
-}) {
-
+async function write({ input, outDir, options }) {
   const jsCodePath = path.join(outDir, 'bundle.js');
 
   const bundle = await rollup({
@@ -21,41 +16,38 @@ async function write({
     plugins: [
       inlinePostCSS({
         styleRegex: /css\`((.|\n)*)\`(;)/gm,
-        plugins:  [
-          require('postcss-rgb-plz')
-        ]
-      })
-    ]
-  })
+        plugins: [require('postcss-rgb-plz')],
+      }),
+    ],
+  });
 
   await bundle.write({
     format: 'esm',
-    file: path.join(outDir, 'bundle.js')
+    file: path.join(outDir, 'bundle.js'),
   });
 
   return {
-      jsCode() {
-        return fs.readFile(jsCodePath, 'utf8')
-      },
-      hasRGBColorValues() {
-        const file = fs.readFileSync(path.join(outDir, 'bundle.js'), 'utf8');
-        const style = file.match(/\`((.|\n)*)\`/gm)[0];
-        return /(#abcefe)/gm.exec(style) == null ? true : false;
-      },
-      isMinified() {
-        const file = fs.readFileSync(path.join(outDir, 'bundle.js'), 'utf8');
-        const style = file.match(/\`((.|\n)*)\`/gm)[0];
-        return /\r|\n/.exec(style) == null ? true : false;
-      }
-  }
-
+    jsCode() {
+      return fs.readFile(jsCodePath, 'utf8');
+    },
+    hasRGBColorValues() {
+      const file = fs.readFileSync(path.join(outDir, 'bundle.js'), 'utf8');
+      const style = file.match(/\`((.|\n)*)\`/gm)[0];
+      return /(#abcefe)/gm.exec(style) == null ? true : false;
+    },
+    isMinified() {
+      const file = fs.readFileSync(path.join(outDir, 'bundle.js'), 'utf8');
+      const style = file.match(/\`((.|\n)*)\`/gm)[0];
+      return /\r|\n/.exec(style) == null ? true : false;
+    },
+  };
 }
 
 test('inline css has rgb values', async () => {
   const res = await write({
     input: 'component.js',
     outDir: 'test/onExtract',
-    options: {}
-  })
-  expect(await res.hasRGBColorValues()).toBe(true)
-})
+    options: {},
+  });
+  expect(await res.hasRGBColorValues()).toBe(true);
+});
